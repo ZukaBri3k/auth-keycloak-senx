3d7ab56c6eb44b39eaf32f63dd97b873e079b173
kdumerchat <kyrill.dumerchat@kwh50.io>
2024-09-04 11:34:41 +0200

    (add) ajout de l'implémentation de la méthode alternative

--- src/hook/useAuth.tsx
+++ src/hook/useAuth.tsx
@@ -12,6 +12,29 @@ export default function useAuth() {
         });
 
         client.init({ onLoad: "login-required", redirectUri: "http://localhost:3000/private" }).then((res) => setIsLogged(res));
+
+        //Version alternative en fetchant un token depuis l'api
+        /* async function login() {
+            const client = await fetch(
+                "https://auth.senx.io/realms/kwh50/protocol/openid-connect/token",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                    body: new URLSearchParams({
+                        client_id: "saas",
+                        username: "kyrill.dumerchat@kwh50.io",
+                        password: "k8?GtA9Kj$RQqndacckE",
+                        grant_type: "password",
+                    }),
+                }
+            );
+
+            return client.json();
+        }
+
+        login().then((res) => console.log(res)); */
     }, []);
 
     return isLogged;
